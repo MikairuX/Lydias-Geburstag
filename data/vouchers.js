93260a0788f129c5ed5db6b83ec3264a85a70581
MikairuX <slaventinskim@gmail.com>
2025-05-28 16:04:03 +0200

    Add files via upload

---
+++ data/vouchers.js
@@ -0,0 +1,45 @@
+export let vouchers;
+
+
+export function loadFromStorage(){
+  vouchers = JSON.parse(localStorage.getItem('vouchers')) 
+
+  if(!vouchers){
+    vouchers = [{
+  name: '1x mit Michael zur Tanzparty',
+  quantity: 3
+  },
+  {
+    name: 'Cola kaufen',
+    quantity : 5
+  },
+  {
+    name: 'Picnicken mit Michael',
+    quantity: 3
+  },
+  {
+    name: 'Meggis ausgegeben bekommen',
+    quantity: 1
+  },
+  {
+    name: 'KFC ausgegenben bekommen',
+    quantity: 1
+  },{
+    name: 'kusseln',
+    quantity: 11957197509175
+  },
+  {
+    name: 'kuss',
+    quantity: 91069286029928
+  },
+   {
+    name: ' 15 min Massage',
+    quantity: 3
+   }
+  ];
+  }
+}
+
+
+
+
